25732c316ab45a860f9c4cb0968e7c395bc407e6
Max Albrecht <1@178.is>
2021-06-20 13:38:08 +0200

    start webfonts demo

---
+++ _web/pages/b612.js
@@ -0,0 +1,9 @@
+import '@fontsource/b612'
+
+export default function page() {
+  return (
+    <div style={{ fontFamily: 'B612' }}>
+      <h1>The quick brown fox jumps over the lazy dog</h1>
+    </div>
+  )
+}
